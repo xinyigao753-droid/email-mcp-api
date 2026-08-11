5b01e85a81f521f03ad4b53f45794227fe3a0923
k4bvt2ff9r-del <k4bvt2ff9r@privaterelay.appleid.com>
2026-08-11 15:48:16 +0800

    Add Access-Control-Allow-Headers to send_mail.js

--- api/send_mail.js
+++ api/send_mail.js
@@ -3,6 +3,7 @@ const nodemailer = require('nodemailer');
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
